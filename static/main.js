2994aa9e974dba69ddfe5c956ca49bf883f68f94
Felippe Scheidt <felippescheidt@gmail.com>
2022-05-21 19:36:59 -0300

    update nav menu

--- static/main.js
+++ static/main.js
@@ -1,9 +1,15 @@
 /* script */
+function loadView(elHash){
+    let target_content = elHash.replace('#','');
+    $('nav a').removeClass('active');
+    $(`a[href='${elHash}']`).addClass('active');
+    $('.content').hide();
+    $(`.${target_content}`).fadeIn(700);
+}
 $(document).ready(function(){
-    $('nav a').click(function(e){
-        let target = $(this).data('el');
-        $('.content,nav a').removeClass('active');
-        $(`.${target}`).addClass('active');
-        $(this).addClass('active');
+    if (window.location.hash.length>1)
+        loadView(window.location.hash);
+    $(window).on('hashchange', function(e){
+        loadView(window.location.hash);
     });
 });
