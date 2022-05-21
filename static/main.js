1b6575698db456454f690d0404f3d88ae7c3e89e
Felippe Scheidt <felippescheidt@gmail.com>
2022-05-21 19:49:44 -0300

    up

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
 /* script */
-function loadView(elHash){
-    console.log(elHash);
+const app = {
+    version: '1.12'
+}
+function loadView(elHash){    
     let target_content = elHash.replace('#','');
     $('nav a').removeClass('active');
     $(`a[href='${elHash}']`).addClass('active');
@@ -8,6 +10,7 @@ function loadView(elHash){
     $(`.${target_content}`).fadeIn(700);
 }
 $(document).ready(function(){
+    $("#app-version").text(app.version);
     if (window.location.hash.length>1)
         loadView(window.location.hash);
     $(window).on('hashchange', function(e){
